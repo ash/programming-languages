662d73d86f641d58d91c934b417e3f02c531c222
Héctor M. Monacci <hector.monacci@gmail.com>
2020-10-22 08:40:12 -0300

    Use current syntax for interpolation
    Change `warn("{}\n", factorial(1));` to `warn("{}\n", .{factorial(1)});` so that the program successfully compiles under current master Zig. A dot and curly braces are added around the function call.

--- Zig/factorial.zig
+++ Zig/factorial.zig
@@ -6,7 +6,7 @@ fn factorial(n: i32) i32 {
 }
 
 pub fn main() void {
-    warn("{}\n", factorial(1)); // 1
-    warn("{}\n", factorial(5)); // 120
-    warn("{}\n", factorial(7)); // 5040
+    warn("{}\n", .{factorial(1)}); // 1
+    warn("{}\n", .{factorial(5)}); // 120
+    warn("{}\n", .{factorial(7)}); // 5040
 }
